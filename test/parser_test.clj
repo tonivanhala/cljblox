60a9c07caabb66ecbe5fcc5f5dfb43621ee0e1b5
Toni Vanhala <toniv@reaktor.fi>
2021-09-27 08:05:58 +0300

    test(parser): add namespace to syntax tree node type

--- test/parser_test.clj
+++ test/parser_test.clj
@@ -2,33 +2,33 @@
   (:require
     [clojure.java.io :as io]
     [scanner :refer [reader->stream-scanner tokens]]
-    [parser]
+    [parser :as p]
     [clojure.test :refer [deftest testing is]]))
 
 (def +equality-expected-tree+
-  {:type :binary :operator :+
+  {:type ::p/BINARY :operator :+
    :left
-   {:type :group
+   {:type ::p/GROUP
     :child
-    {:type :binary
+    {:type ::p/BINARY
      :operator :-
-     :left {:type :decimal :value 5.0}
+     :left {:type ::p/DECIMAL :value 5.0}
      :right
-     {:type :group
+     {:type ::p/GROUP
       :child
-      {:type :binary :operator :-
-       :left {:type :decimal :value 3.0}
-       :right {:type :decimal :value 1.0}}}}}
+      {:type ::p/BINARY :operator :-
+       :left {:type ::p/DECIMAL :value 3.0}
+       :right {:type ::p/DECIMAL :value 1.0}}}}}
    :right
-   {:type :group
+   {:type ::p/GROUP
     :child
-    {:type :unary :operator :-
-     :child {:type :decimal :value 1.0}}}})
+    {:type ::p/UNARY :operator :-
+     :child {:type ::p/DECIMAL :value 1.0}}}})
 
 (deftest parse-numbers
   (testing "extracts number tokens from a file"
     (let [test-file (io/reader (io/file (io/resource "numbers.blox")))
           scanner (reader->stream-scanner test-file)
           tokens-from-file (tokens scanner)
-          syntax-tree (parser/parse tokens-from-file)]
+          syntax-tree (p/parse tokens-from-file)]
       (is (= +equality-expected-tree+ syntax-tree)))))
