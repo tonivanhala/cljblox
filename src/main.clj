3bbfd9b9ff39c85615c2c3ed6a711ba519c5ef26
Toni Vanhala <toniv@reaktor.fi>
2022-01-02 16:48:30 +0200

    feat(interpreter): run interpreter on files and repl input

--- src/main.clj
+++ src/main.clj
@@ -4,11 +4,19 @@
     [clojure.java.io :as io]
     [clojure.string :as str]
     [clojure.tools.cli :refer [parse-opts]]
-    [scanner :refer [reader->stream-scanner tokens]])
-  (:import (java.io ByteArrayInputStream InputStreamReader BufferedReader)))
+    [interpreter :as i]
+    [parser :as p]
+    [scanner :refer [reader->stream-scanner tokens]]
+    [tokens :as t])
+  (:import (java.io ByteArrayInputStream InputStreamReader BufferedReader)
+           (clojure.lang ExceptionInfo)))
 
 (def +cli-options+
   [])
+
+(def +initial-prompt-state+
+  {:open-parens '()
+   :tokens []})
 
 (defn print-banner
   []
@@ -25,19 +33,72 @@
       "BBBB  LLLL  OOO  XX XX"
       "Blox interpreter - \"Bacon-flavored Lox\"")))
 
+(defn block-open?
+  [{:keys [open-parens]}]
+  (seq open-parens))
+
 (defn show-prompt
   ([]
-   (show-prompt false))
-  ([is-block-open]
-   (if is-block-open
+   (show-prompt +initial-prompt-state+))
+  ([prompt-state]
+   (if (block-open? prompt-state)
      (print "... ")
      (print "> "))
-   (flush)))
+   (flush)
+   prompt-state))
+
+(defn process-line
+  [prompt-state]
+  (if (not (block-open? prompt-state))
+    (do
+      (some->
+        prompt-state
+        :tokens
+        (p/parse)
+        (i/eval-node)
+        (println))
+      +initial-prompt-state+)
+    prompt-state))
+
+(defn closing-paren?
+  [{prev :type} {this :type}]
+  (or
+    (and (= ::t/LEFT-PAREN prev) (= ::t/RIGHT-PAREN this))
+    (and (= ::t/LEFT-BRACE prev) (= ::t/RIGHT-BRACE this))))
+
+(defn process-paren
+  [{:keys [open-parens] :as prompt-state} paren]
+  (if (t/open-paren? paren)
+    (-> prompt-state
+        (update :open-parens conj paren)
+        (update :tokens conj paren))
+    (let [latest-paren (first open-parens)]
+      (if (closing-paren? latest-paren paren)
+        (-> prompt-state
+            (update :open-parens rest)
+            (update :tokens conj paren))
+        (throw (ex-info "Mismatched parenthesis." {:token paren}))))))
 
 (defn process-stream
-  [scanner]
-  (doseq [token (tokens scanner)]
-    (println token)))
+  [scanner prompt-state]
+  (reduce
+    (fn [-prompt-state {:keys [type] :as token}]
+      (cond
+        (t/paren? token) (process-paren -prompt-state token)
+        (not= ::t/EOF type) (update -prompt-state :tokens conj token)
+        :else -prompt-state))
+    prompt-state
+    (tokens scanner)))
+
+(defn safe-process-stream
+  "Process stream while catching and printing out exceptions"
+  [& args]
+  (try
+    (apply process-stream args)
+    (catch ExceptionInfo e
+      (println (ex-message e))
+      (println (ex-data e))
+      +initial-prompt-state+)))
 
 (defn line->reader
   [^String line]
@@ -53,18 +114,30 @@
 
 (defn run-prompt
   []
-  (show-prompt)
-  (loop [scanner (some-> (get-user-input) (reader->stream-scanner))]
+  (loop [prompt-state (show-prompt)
+         scanner (some-> (get-user-input) (reader->stream-scanner))]
     (when (some? scanner)
-      (process-stream scanner)
-      (show-prompt)
-      (recur (some-> (get-user-input) (reader->stream-scanner scanner))))))
+      (let [-prompt-state (safe-process-stream scanner prompt-state)]
+        (-> -prompt-state
+            (process-line)
+            (show-prompt)
+            (recur
+              (some-> (get-user-input) (reader->stream-scanner scanner))))))))
 
 (defn exec-file
   [filename]
   (with-open [reader (io/reader (io/file filename))]
-    (let [scanner (reader->stream-scanner reader)]
-      (process-stream scanner))))
+    (let [scanner (reader->stream-scanner reader)
+          result (process-stream scanner +initial-prompt-state+)]
+      (when (block-open? result)
+        (throw
+          (ex-info
+            "Unclosed block in file."
+            {:token (first (:open-parens result))})))
+      (-> result
+          :tokens
+          p/parse
+          i/eval-node))))
 
 (defn -main
   [& args]
--- src/tokens.clj
+++ src/tokens.clj
@@ -2,3 +2,17 @@
 
 (defrecord Token
   [type lexeme literal line column])
+
+(defn paren?
+  [{:keys [type]}]
+  (or
+    (= ::LEFT-BRACE type)
+    (= ::RIGHT-BRACE type)
+    (= ::LEFT-PAREN type)
+    (= ::RIGHT-PAREN type)))
+
+(defn open-paren?
+  [{:keys [type]}]
+  (or
+    (= ::LEFT-BRACE type)
+    (= ::LEFT-PAREN type)))
